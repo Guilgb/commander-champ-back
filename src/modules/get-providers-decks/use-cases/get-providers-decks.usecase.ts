d8d8e425b3891ec5cc81be3fba8d82d802ccfe03
Guilherme <guilherme.gb@hotmail.com>
2025-01-15 21:01:54 -0300

    feat: aprimora o uso do serviço Moxfield e normaliza dados de decks

--- src/modules/get-providers-decks/use-cases/get-providers-decks.usecase.ts
+++ src/modules/get-providers-decks/use-cases/get-providers-decks.usecase.ts
@@ -18,40 +18,58 @@ export class GetProvidersDecksUseCase {
       if (input.provider == 'topdeckgg') {
         const topDeckUrl = input.url;
         const topDeckService = await this.topdeckggService.getTopDecks(topDeckUrl);
-        const deckList = await this.moxfieldService.getMoxfieldDeck('https://moxfield.com/decks/CeUYdgT8OEuYisHhMD_4CQ');
+        topDeckService.map(async (deck: TopDeck) => {
 
-        const {
-          id,
-          name,
-          format,
-          commanders
-        } = deckList;
-        // console.log(id, name, format, commanders);
-        console.log(this.normalizeDeckData(commanders));
-        return deckList;
-      }[
+          const deckList = await this.moxfieldService.getMoxfieldDeck(deck.decklist);
+          console.log(deckList);
+          const {
+            name,
+            format,
+            commanders,
+            mainboard
+          } = deckList;
 
-      ]
+          const maindeck = this.normalizeDeckData(mainboard);
+          const commander = this.normalizeDeckData(commanders);
+          let color_identity = [];
+
+          for (const ci in commander) {
+            color_identity.push(commander[ci].card.color_identity);
+          }
+
+          return {
+            name,
+            format,
+            commanders: commander,
+            color_identity: color_identity.flat(),
+            deck: maindeck
+          };
+        });
+      }
     } catch (error) {
       throw new Error(error);
     }
   }
   private normalizeDeckData(data: any): any[] {
-    const normalizedData = Object.keys(data).map(key => {
-      const card = data[key];
-      return {
-        name: key,
-        card: {
-          name: card.card.name,
-          cmc: card.card.cmc,
-          type: card.card.type,
-          mana_cost: card.card.mana_cost,
-          colors: card.card.colors,
-          color_identity: card.card.color_identity,
-        }
-      };
-    });
-
-    return normalizedData;
+    try {
+      const normalizedData = Object.keys(data).map(key => {
+        const card = data[key];
+        return {
+          name: key,
+          card: {
+            name: card.card.name,
+            cmc: card.card.cmc,
+            type: card.card.type,
+            mana_cost: card.card.mana_cost,
+            colors: card.card.colors,
+            color_identity: card.card.color_identity,
+          }
+        };
+      });
+      return normalizedData;
+    } catch (error) {
+      console.log(data);
+      throw new Error(error);
+    }
   }
 }
