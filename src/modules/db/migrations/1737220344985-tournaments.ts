4c3763947813826f6ff9dd27f58dca613b663eb2
Guilherme <guilherme.gb@hotmail.com>
2025-01-18 15:12:46 -0300

    feat: cria migrações para tabelas de decks, cartas e torneios com relacionamentos adequados

--- src/modules/db/migrations/1737220344985-tournaments.ts
+++ src/modules/db/migrations/1737220344985-tournaments.ts
@@ -2,10 +2,21 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Tournaments1737220344985 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.query(`
+      CREATE TABLE tournaments (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        start_date DATE NOT NULL,
+        end_date DATE NOT NULL,
+        format VARCHAR(255) NOT NULL CHECK (format IN ('c500', 'cedh', 'casual', 'conquest'))
+      );
+    `)
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE tournaments`);
+  }
 
 }
